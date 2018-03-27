fb1d39145560c4bbc829e64ef41c7a718f6e2c81
Unknown <33198805+allesgeklaut@users.noreply.github.com>
2018-03-27 22:25:54 +0200

    database is now running remotely; And some formatting

--- KRJOTurk/src/Controllers/HomeController.java
+++ KRJOTurk/src/Controllers/HomeController.java
@@ -1,84 +1,80 @@
 package Controllers;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.text.Text;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
 import service.LearningInstance;
 import service.currentUser;
 import service.systemData;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-
 /**
  * Created by a1 on 28.10.17.
  */
-public class HomeController extends Controller{
-    @FXML
-    ChoiceBox<String> searchChoiceBox;
-    @FXML
-    TextField searchTextField;
-    @FXML
-    ListView<LearningInstance> searchListView;
-    @FXML
-    TreeView<LearningInstance> homeTree;
+public class HomeController extends Controller {
+	@FXML
+	ChoiceBox<String> searchChoiceBox;
+	@FXML
+	TextField searchTextField;
+	@FXML
+	ListView<LearningInstance> searchListView;
+	@FXML
+	TreeView<LearningInstance> homeTree;
 
+	@FXML
+	private void initialize() throws SQLException {
+		viewInit();
+		statementLabel.setText("Welcome Mr. " + currentUser.getInstance().getUser_surname());
+		homeButton.setDisable(true);
+		buildTree(homeTree);
+		choiceBoxInitializer();
+	}
 
+	public void searchOnAction() {
+		String searchText = searchTextField.getText();
+		ArrayList<LearningInstance> output = new ArrayList<>();
+		switch (getChoice()) {
+		case "Learning Categories": {
+			output = systemData.getInstance().search(2, searchText);
+			break;
+		}
+		case "Learning Applications": {
+			output = systemData.getInstance().search(1, searchText);
+			break;
+		}
+		case "Learning Units": {
+			output = systemData.getInstance().search(3, searchText);
+		}
+		}
+		initializeCListView(output);
+	}
 
+	private void initializeCListView(ArrayList<LearningInstance> searchResult) {
+		searchListView.getItems().clear();
+		ObservableList<LearningInstance> observableList = searchListView.getItems();
+		for (LearningInstance li : searchResult) {
+			observableList.add(li);
+		}
+		searchListView.getSelectionModel().select(0);
+		searchListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+	}
 
-    @FXML
-    private void initialize() throws SQLException {
-        viewInit();
-        statementLabel.setText("Welcome Mr. " + currentUser.getInstance().getUser_surname());
-        homeButton.setDisable(true);
-        buildTree(homeTree);
-        choiceBoxInitializer();
-        System.out.println("test");
-    }
+	// Return the searching area from ChoiceBox
+	private String getChoice() {
+		return searchChoiceBox.getValue();
+	}
 
-    public void searchOnAction() {
-        String searchText = searchTextField.getText();
-        ArrayList<LearningInstance> output = new ArrayList<>();
-        switch (getChoice()) {
-            case "Learning Categories" : {
-                output = systemData.getInstance().search(2, searchText);
-                break;
-            }
-            case "Learning Applications" : {
-                output = systemData.getInstance().search(1, searchText);
-                break;
-            }
-            case "Learning Units" : {
-                output = systemData.getInstance().search(3, searchText);
-            }
-        }
-        initializeCListView(output);
-    }
+	// INITIALIZE FUNCTIONS :
 
-    private void initializeCListView(ArrayList<LearningInstance> searchResult) {
-        searchListView.getItems().clear();
-        ObservableList<LearningInstance> observableList = searchListView.getItems();
-        for(LearningInstance li : searchResult) {
-            observableList.add(li);
-        }
-        searchListView.getSelectionModel().select(0);
-        searchListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-    }
-
-
-    //Return the searching area from ChoiceBox
-    private String getChoice() {
-        return searchChoiceBox.getValue();
-    }
-
-
-    //INITIALIZE FUNCTIONS :
-
-    //Initialize choiceBox
-    private void choiceBoxInitializer() {
-        searchChoiceBox.getItems().addAll("Learning Applications", "Learning Categories",
-                "Learning Units");
-        searchChoiceBox.setValue("Learning Applications");
-    }
+	// Initialize choiceBox
+	private void choiceBoxInitializer() {
+		searchChoiceBox.getItems().addAll("Learning Applications", "Learning Categories", "Learning Units");
+		searchChoiceBox.setValue("Learning Applications");
+	}
 }
--- KRJOTurk/src/service/DBConnector.java
+++ KRJOTurk/src/service/DBConnector.java
@@ -1,43 +1,95 @@
 package service;
 
-
-import com.mysql.fabric.jdbc.FabricMySQLDriver;
-
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//Connect local MySQL Database to the program
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+//Connect remote MySQL Database to the program through ssh
 public class DBConnector {
-    private static final String URL = "jdbc:mysql://localhost:3306/OADTurk?autoReconnect=true&useSSL=false";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
 
-    private Connection connection;
+	// local database data (currently running on pi)
+	private static final String URL = "jdbc:mysql://localhost:4321/OADTurk?autoReconnect=true&useSSL=false";
+	private static final String USERNAME = "superUser";
+	private static final String PASSWORD = "MC_1919";
 
-    public Connection getConnection() {
-        return connection;
-    }
+	/**
+	 * connects to the remote server over ssh at port 22
+	 * 
+	 * @param host_
+	 *            is the the remote host in form of user@host
+	 * @param lportRhostRport
+	 *            a string of localPort:RemoteHost:RemotePort; RemoteHost and
+	 *            RemotePort are values used on the remote system
+	 */
+	public static void connectSSH(String host_, String lportRhostRport) {
 
-    public void setConnection(Connection connection) {
-        this.connection = connection;
-    }
+		int lport;
+		String rhost;
+		int rport;
 
-    public DBConnector() {}
+		try {
+			JSch jsch = new JSch();
 
-    public void connectToDB(){
-        connect(URL, USERNAME, PASSWORD);
+			// parse host and user
+			String host = host_;
+			String user = host.substring(0, host.indexOf('@'));
+			host = host.substring(host.indexOf('@') + 1);
 
-    }
+			Session session = jsch.getSession(user, host, 22);
 
-    public void connect(String url, String username, String password){
-        try {
-            Driver driver = new FabricMySQLDriver();
-            DriverManager.registerDriver(driver);
-            connection = DriverManager.getConnection(url, username, password );
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+			// parse lportRhostRport
+			String foo = lportRhostRport;
+			lport = Integer.parseInt(foo.substring(0, foo.indexOf(':')));
+			foo = foo.substring(foo.indexOf(':') + 1);
+			rhost = foo.substring(0, foo.indexOf(':'));
+			rport = Integer.parseInt(foo.substring(foo.indexOf(':') + 1));
+
+			// set password of ssh
+			session.setPassword("MC_1919");
+			session.setConfig("StrictHostKeyChecking", "no");
+
+			session.connect();
+
+			// TODO KRJO port cannot be bound twice; e.g. when changing something in
+			// settings
+			int assinged_port = session.setPortForwardingL(lport, rhost, rport);
+			System.out.println("localhost:" + assinged_port + " -> " + rhost + ":" + rport);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private Connection connection;
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+	public DBConnector() {
+	}
+
+	public void connectToDB() {
+		connectSSH("pi@192.168.8.107", "4321:localhost:3306");
+		connect(URL, USERNAME, PASSWORD);
+
+	}
+
+	public void connect(String url, String username, String password) {
+		try {
+			Driver driver = new FabricMySQLDriver();
+			DriverManager.registerDriver(driver);
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
