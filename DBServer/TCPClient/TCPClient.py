325f44ff7b15bf816e6621f12bb3eb21dfe57cbb
Unknown <kremser.johannes@gmail.com>
2018-03-30 23:27:31 +0200

    Continued work on DBServerApp

--- DBServer/TCPClient/TCPClient.py
+++ DBServer/TCPClient/TCPClient.py
@@ -8,9 +8,10 @@ Test client or the TCP Server
 import socket
 
 TCP_IP = "127.0.0.1"
-TCP_PORT = 5005
+TCP_PORT = 8080
 BUFFER_SICE = 1024
-MESSAGE = b'Hello World'
+MESSAGE = str('{0:04b}'.format(0)) + "SELECT * FROM learning_units            "
+MESSAGE = MESSAGE.encode()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
--- DBServer/TCPServer/TCPServer.py
+++ DBServer/TCPServer/TCPServer.py
@@ -4,31 +4,23 @@ Created on 29.03.2018
 @author: krems
 '''
 import socket
+import asyncore
 
 
-class TCPServer(object):
+class TCPServer(asyncore.dispatcher):
 
     def __init__(self, port):
+        asyncore.dispatcher.__init__(self)
         self.TCP_IP = '127.0.0.1'
         self.TCP_PORT = port
-        self.BUFFER_SICE = 1024
+        self.open()
 
     def open(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind((self.TCP_IP, self.TCP_PORT))
-        self.s.listen(1)
-        print('\n Server started...')
-        
-        self.conn, addr = self.s.accept()
-        print('\n Connection address: ', addr)
-        while True:
-            data = self.conn.recv(self.BUFFER_SICE)
-            if not data: break
-            print('\n received data: ', data) 
-            # TODO handle incoming commands correct
-            self.conn.send(data)
-        self.conn.close()
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind((self.TCP_IP, self.TCP_PORT))
+        self.listen(5)  # TODO find out what the int stands for
+        print('\n Server started...')   
 
     def close(self):
-        self.conn.close()
+        pass
 
