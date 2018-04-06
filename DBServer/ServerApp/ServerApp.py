7e2d4a878a2818462c159f637284de0adc5f8693
allesgeklaut <33198805+allesgeklaut@users.noreply.github.com>
2018-04-06 16:03:25 +0200

    updated python server: reseaves now every data package and small changes

--- DBServer/ServerApp/ServerApp.py
+++ DBServer/ServerApp/ServerApp.py
@@ -8,6 +8,10 @@ from TCPServer.TCPServer import TCPServer
 import asyncore
 from DBConnector.DBConnector import DBConnector
 from enum import Enum
+from _mysql_exceptions import MySQLError
+import numpy as np
+import cv2
+import os.path
 
 
 # handles database requests and echos the result
@@ -17,29 +21,100 @@ class EchoHandler(asyncore.dispatcher_with_send):
         asyncore.dispatcher_with_send.__init__(self, sock)
         self.db = DBConnector('root', 'root')
         self.db.connect()
+        self.headerSize = 4  # size of data size header and CID header
+        self.commandBytes = 64  # size of SQL Command placeholder
+        self.dataBuffer = b""  
+        self.dataSize = 0        
 
+    # gets called on new data 
     def handle_read(self):
-        data = self.recv(16384)
+        data = self.recv(1024)
+        rawData = None
+        status = None
         if data:
-            CID, sqlCommand, imageData = self.parser(data)
-            if commandID(CID) == commandID.getData:
-                self.send(self.getData(sqlCommand))
+            if self.dataSize == 0:
+                self.dataSize = self.parseDataSize(data[0:self.headerSize])
+                self.dataBuffer += data[self.headerSize:len(data)]
+            else:
+                self.dataBuffer += data
+            
+            if len(self.dataBuffer) >= self.dataSize:
+                CID, command, imageData = self.parser(self.dataBuffer)
+                if commandID(CID) == commandID.getData:
+                    status, rawData = self.getData(command)
+                elif commandID(CID) == commandID.setData:
+                    status, rawData = self.setData(command)
+                elif commandID(CID) == commandID.uploadImage:
+                    status, rawData = self.uploadImage(imageData, command)
+                elif commandID(CID) == commandID.downloadImage:
+                    status, rawData = self.downloadImage(command)
+                rawData = str(status).encode() + rawData
                 
-    commandIDBytes = 4
-    commandBytes = 64
+                # header with size for echo  
+                dataLength = len(rawData)
+                dl = [0] * self.headerSize
+                dl[0] = dataLength & 0xff
+                dl[1] = (dataLength >> 8) & 0xff
+                dl[2] = (dataLength >> 16) & 0xff
+                dl[3] = (dataLength >> 24) & 0xff 
+                dl = list(reversed(dl))
+                BDataLength = bytes(dl)
+                
+                # send back data with header
+                self.send(BDataLength + rawData)
         
     # get data from database
     def getData(self, SQLcommand):
-        c = self.db.connection.cursor()
-        c.execute(SQLcommand)
-        tup = c.fetchall()
-        # serialize all tuples received from database
-        out = ''
-        for k in range(0, len(tup)):
-            out += self.serialize(tup[k])
-        # return in bytes
-        return out.encode()
+        try:
+            c = self.db.connection.cursor()
+            c.execute(SQLcommand)
+            tup = c.fetchall()
+            # serialize all tuples received from database
+            out = ''
+            for k in range(0, len(tup)):
+                out += self.serialize(tup[k])
+            # return in bytes
+            c.close()
+            return 0, out.encode()
+        except MySQLError as e:
+            return -1, b"MYSQL error: " + repr(e).encode()
+        except:
+            return -1, ''
     
+    def setData(self, SQLcommand):
+        try:
+            c = self.db.connection.cursor()
+            c.execute(SQLcommand)
+            self.db.connection.commit()
+            c.close()
+            return 0, b""
+        except MySQLError as e:
+            return -1, b"MYSQL error: " + repr(e).encode()
+        except:
+            return -1, b""
+        
+    def uploadImage(self, image, command):
+        # saves foto on filesystem; saves nothing into database
+        try:
+            fotoName = command
+            nparr = np.fromstring(image, np.uint8)
+            img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            path = os.path.join("ServerData", fotoName + ".jpg")
+            cv2.imwrite(path, img_np)
+            return 0, fotoName.encode()
+        except ... as e: 
+            return -1, b"Error: " + repr(e).encode()
+
+    def downloadImage(self, command):
+        try:
+            fotoName = command
+            path = os.path.join("ServerData", fotoName + ".jpg")
+            img_np = cv2.imread(path)
+            _, img_array = cv2.imencode(".jpg", img_np)  
+            return 0, np.ndarray.tobytes(img_array)
+        except ... as e:
+            return -1, b"Error: " + repr(e).encode()
+
     # serialize a tuple from database into string
     def serialize(self, inTup):
         out = '('
@@ -49,29 +124,23 @@ class EchoHandler(asyncore.dispatcher_with_send):
         out = out[0:-1]
         out += ')'
         return out
-    
-    def setData(self, SQLCommand):
-        pass
-    
-    def uploadImage(self, image, SQLCommand):
-        pass
-    
-    def downloadImage(self, SQLCommand):
-        pass
         
     # parses the commandstring from client
     def parser(self, inString):
         commandString = inString
         
-        commandID = commandString[0:self.commandIDBytes]
-        commandID = int(commandID, 2)
-        commandString = commandString[self.commandIDBytes:len(commandString)]
+        commandID = commandString[0:self.headerSize]
+        commandID = int.from_bytes(commandID, byteorder='big')
+        commandString = commandString[self.headerSize:len(commandString)]
         if (len(commandString) >= self.commandBytes):
             sqlCommand = commandString[0:self.commandBytes]
             imageData = commandString[self.commandBytes:len(commandString)]
-            return commandID, sqlCommand.decode("utf-8") , imageData
+            return commandID, sqlCommand.decode().strip("\00x") , imageData
         else:
-            return commandID, commandString.decode("utf-8") , ''
+            return commandID, commandString.decode().strip() , ''
+        
+    def parseDataSize(self, inString):
+        return int.from_bytes(inString, byteorder='big')
 
 
 class srv(TCPServer):
--- KRJOTurkTCPCommunication/src/TCPComm/TCPClient.java
+++ KRJOTurkTCPCommunication/src/TCPComm/TCPClient.java
@@ -1,52 +1,94 @@
 package TCPComm;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 //TCPClient.java
 //A client program implementing TCP socket
-import java.net.*; 
-import java.io.*; 
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
-public class TCPClient { 
-	public static void main (String args[]) 
-	{// arguments supply message and hostname of destination  
-		Socket s = null; 
-		try{ 
-		  int serverPort = 6880;
-			  String ip = "localhost";
-			  String data = "Hello, How are you?"; 
-			  
-		  s = new Socket(ip, serverPort); 
-		  DataInputStream input = new DataInputStream( s.getInputStream()); 
-		  DataOutputStream output = new DataOutputStream( s.getOutputStream()); 
-		  
-			  //Step 1 send length
-			  System.out.println("Length"+ data.length());
-			  output.writeInt(data.length());
-			  //Step 2 send length
-			  System.out.println("Writing.......");
-			  output.writeBytes(data); // UTF is a string encoding
-			  
-			  //Step 1 read length
-			  int nb = input.readInt();
-			  byte[] digit = new byte[nb];
-			  //Step 2 read byte
-			  for(int i = 0; i < nb; i++)
+import javax.imageio.ImageIO;
+
+public class TCPClient {
+	public static void main(String args[]) {// arguments supply message and hostname of destination
+		Socket s = null;
+		try {
+			int serverPort = 8080;
+			String ip = "localhost";
+			int CID = 2;
+			// String SQLCommand = "SELECT * FROM users";
+			// String data = "0001" + "UPDATE users SET `email`='asd' WHERE user_id=11";
+			String SQLCommand = "image01";
+
+			// Bild
+			BufferedImage image = ImageIO.read(new File("C:\\Users\\krems\\Pictures\\meli.jpg"));
+
+			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+			if (CID == 2)
+				ImageIO.write(image, "jpg", byteArrayOutputStream);
+
+			byte[] CIDbytes = ByteBuffer.allocate(4).putInt(CID).array();
+			byte[] SQLbytes = SQLCommand.getBytes(StandardCharsets.UTF_8);
+			byte[] SQLByteBuffer = ByteBuffer.allocate(64).put(SQLbytes).array();
+			// get data size in bytes
+			int dataSize = CIDbytes.length + SQLByteBuffer.length + byteArrayOutputStream.toByteArray().length;
+			byte[] dataSizeBytes = ByteBuffer.allocate(4).putInt(dataSize).array();
+
+			// String data = bytes.toString() + byteArrayOutputStream.toString();
+			s = new Socket(ip, serverPort);
+			DataInputStream input = new DataInputStream(s.getInputStream());
+			DataOutputStream output = new DataOutputStream(s.getOutputStream());
+
+			System.out.println("Writing.......");
+			output.write(dataSizeBytes);
+			output.write(CIDbytes);
+			output.write(SQLByteBuffer);
+			if (CID == 2)
+				output.write(byteArrayOutputStream.toByteArray());
+			output.flush();
+
+			// Step 1 read length
+			int nb = input.readInt();
+			byte[] digit = new byte[nb];
+			// Step 2 read byte
+			for (int i = 0; i < nb; i++)
 				digit[i] = input.readByte();
-		  
-			  String st = new String(digit);
-		  System.out.println("Received: "+ st); 
+			// input.read(digit, 0, nb);
+
+			if (CID == 3) {
+				byte[] imgBytes = Arrays.copyOfRange(digit, 1, digit.length);
+				BufferedImage img = ImageIO.read(new ByteArrayInputStream(imgBytes));
+				OutputStream fileOut = new FileOutputStream("image.jpg");
+				ImageIO.write(img, "jpg", fileOut);
+				fileOut.close();
+			} else {
+				String st = new String(digit);
+				System.out.println("Received: " + st);
+			}
+			System.out.println("end");
+		} catch (UnknownHostException e) {
+			System.out.println("Sock:" + e.getMessage());
+		} catch (EOFException e) {
+			System.out.println("EOF:" + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("IO:" + e.getMessage());
+		} finally {
+			if (s != null)
+				try {
+					s.close();
+				} catch (IOException e) {
+					/* close failed */}
 		}
-		catch (UnknownHostException e){ 
-			System.out.println("Sock:"+e.getMessage());}
-		catch (EOFException e){
-			System.out.println("EOF:"+e.getMessage()); }
-		catch (IOException e){
-			System.out.println("IO:"+e.getMessage());} 
-		finally {
-			  if(s!=null) 
-				  try {s.close();
-				  } 
-				  catch (IOException e) {/*close failed*/}
 	}
 }
-}
-
